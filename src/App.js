43d5609d944a4fae2536fa39d64839675041e927
danlin0226 <danlin0226@gmail.com>
2022-11-21 15:57:27 -0800

    set up basic route template

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
-      <p>Hello</p>
-    </div>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/warehouses">
+          <Route index element={<WarehousesPage />} />
+          <Route path=":warehouseId" element={<WarehousesPage />} />
+        </Route>
+        <Route path="/inventory" element={<InventoryPage />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </>
   );
 }
 
