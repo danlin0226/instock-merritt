3e841d79f43937c78197f7d713541b8cc07e2961
super_fox <super_fox@mail.ru>
2022-11-21 16:47:53 -0800

    some changes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ function Footer(){
 return(
     <>
     <footer className="footer">
-        <div className="footer__text" >InStock Inc. All Right Reserved.</div>
+        <div className="footer__text" >InStock Inc. All Right Reserved..</div>
     </footer>
     </>
 )
