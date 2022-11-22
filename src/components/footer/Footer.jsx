0cfa1aced261e4618ee27e2113cb745b14a53637
super_fox <super_fox@mail.ru>
2022-11-22 15:54:36 -0800

    footer export fixed

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,14 +1,12 @@
-import "./Footer.scss"
+import "./Footer.scss";
+import React from "react";
 
-function Footer(){
-return(
-    <>
+function Footer() {
+  return (
     <footer className="footer">
-        <div className="footer__text" >InStock Inc. All Right Reserved.</div>
+      <div className="footer__text">InStock Inc. All Right Reserved.</div>
     </footer>
-    </>
-)
-
+  );
 }
 
-export default Footer();
+export default Footer;
