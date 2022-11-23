22c070ca5b60daaf0987a6f13bc6be609431bb71
danlin0226 <danlin0226@gmail.com>
2022-11-22 18:25:15 -0800

    refactor warehouseDetails and listingCard

--- src/components/listingsCard/ListingsCard.jsx
+++ src/components/listingsCard/ListingsCard.jsx
@@ -15,39 +15,33 @@ const ListingsCard = () => {
         <div>QTY</div>
         <div>ACTIONS</div>
       </div>
-      <div className="listingsCard__text-cont">
-        <div className="listingsCard__cont-left">
-          <div className="listingsCard__cont">
-            <h4 className="listingsCard__title">INVENTORY ITEM</h4>
-            <h3 className="listingsCard__text listingsCard__text--blue">
-              Television{" "}
-              <img
-                className="listingsCard__chev"
-                src={chevronIcon}
-                alt="chevron"
-              />
-            </h3>
-          </div>
-          <div className="listingsCard__cont">
-            <h4 className="listingsCard__title">CATEGORY</h4>
-            <p className="listingsCard__text">Electronics</p>
-          </div>
-        </div>
-        <div className="listingsCard__cont-right">
-          <div className="listingsCard__cont">
-            <h4 className="listingsCard__title">STATUS</h4>
-            {/* conditional for modifier needed */}
-            <h4 className="listingsCard__text listingsCard__text--inStock">
-              IN STOCK
-            </h4>
-          </div>
-          <div className="listingsCard__cont">
-            <h4 className="listingsCard__title">QTY</h4>
-            <p className="listingsCard__text">500</p>
-          </div>
-        </div>
+      <div className="listingsCard__cont listingsCard__cont--first">
+        <h4 className="listingsCard__title">INVENTORY ITEM</h4>
+        <h3 className="listingsCard__text listingsCard__text--blue">
+          Television{" "}
+          <img className="listingsCard__chev" src={chevronIcon} alt="chevron" />
+        </h3>
       </div>
-      <div className="listingsCard__icon-cont">
+
+      <div className="listingsCard__cont">
+        <h4 className="listingsCard__title">CATEGORY</h4>
+        <p className="listingsCard__text">Electronics</p>
+      </div>
+
+      <div className="listingsCard__cont listingsCard__cont--second">
+        <h4 className="listingsCard__title">STATUS</h4>
+        {/* conditional for modifier needed */}
+        <h4 className="listingsCard__text listingsCard__text--inStock">
+          IN STOCK
+        </h4>
+      </div>
+
+      <div className="listingsCard__cont">
+        <h4 className="listingsCard__title">QTY</h4>
+        <p className="listingsCard__text">500</p>
+      </div>
+
+      <div className="listingsCard__cont">
         <img src={trashIcon} alt="trashbin" />
         <img src={editIcon} alt="pencil" />
       </div>
--- src/components/warehouse-details/WarehouseDetails.jsx
+++ src/components/warehouse-details/WarehouseDetails.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ListingsCard from "../listingsCard/ListingsCard";
 
 import "./WarehouseDetails.scss";
@@ -7,30 +7,30 @@ const warehouseDetails = () => {
   return (
     <div className="warehouseDetails">
       <section className="warehouseDetails__head">
-        <div className="warehouseDetails__top">
-          <div className="warehouseDetails__cont">
-            <h4 className="warehouseDetails__title">WAREHOUSE ADDRESS:</h4>
-            <p className="warehouseDetails__text">
-              33 Pearl Street SW conditional here
-            </p>
-            <p className="warehouseDetails__text">display none mod</p>
-          </div>
+        <div className="warehouseDetails__cont">
+          <h4 className="warehouseDetails__title">WAREHOUSE ADDRESS:</h4>
+          <p className="warehouseDetails__text">
+            33 Pearl Street SW conditional here
+          </p>
+          <p className="warehouseDetails__text">display none mod</p>
         </div>
-        <div className="warehouseDetails__bottom">
-          <div className="warehouseDetails__cont">
-            <h4 className="warehouseDetails__title">CONTACT NAME:</h4>
-            <p className="warehouseDetails__text">Graeme Lyon</p>
-            <p className="warehouseDetails__text"> Warehouse Manager</p>
-          </div>
-          <div className="warehouseDetails__cont">
-            <h4 className="warehouseDetails__title">CONTACT INFORMATION:</h4>
-            <p className="warehouseDetails__text">+123-465-7897</p>
-            <p className="warehouseDetails__text">asdfsa@instock.com</p>
-          </div>
+
+        <div className="warehouseDetails__cont">
+          <h4 className="warehouseDetails__title">CONTACT NAME:</h4>
+          <p className="warehouseDetails__text">Graeme Lyon</p>
+          <p className="warehouseDetails__text"> Warehouse Manager</p>
+        </div>
+        <div className="warehouseDetails__cont">
+          <h4 className="warehouseDetails__title">CONTACT INFORMATION:</h4>
+          <p className="warehouseDetails__text">+123-465-7897</p>
+          <p className="warehouseDetails__text">asdfsa@instock.com</p>
         </div>
       </section>
       {/* <section>labels</section> */}
       <section>
+        <ListingsCard />
+        <ListingsCard />
+        <ListingsCard />
         <ListingsCard />
         <ListingsCard />
       </section>
