d8d0bc1e82d98d3b48c9b6ab16428bca8c9a6d06
super_fox <super_fox@mail.ru>
2022-11-22 19:35:42 -0800

    footer © add

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,7 +4,7 @@ import React from "react";
 function Footer() {
   return (
     <footer className="footer">
-      <div className="footer__text">InStock Inc. All Right Reserved.</div>
+      <div className="footer__text">© InStock Inc. All Right Reserved.</div>
     </footer>
   );
 }
