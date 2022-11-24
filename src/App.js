0b8f2d6b0f0408520d5ba2ffbb29407228492cd6
wilsonn-g <wilson-gee@hotmail.com>
2022-11-24 06:21:42 -0800

    rebasing and pulling dev changes

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import EditWarehouse from "./pages/EditWarehouse";
 
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
+import InventoryForm from "./components/inventory-form/InventoryForm";
 
 function App() {
   return (
@@ -17,9 +18,15 @@ function App() {
         <Route path="*" element={<WarehousesPage />} />
         <Route path="/warehouses/*" element={<WarehousesPage />} />
         <Route path="/warehouse-add" element={<AddWarehouse />} />
-        <Route path="/warehouse-edit" element={<EditWarehouse />} />
-        <Route path="/inventory" element={<InventoryPage />} />
-        {/* <Route path="*" element={<Navigate to="/" />} /> */}
+        <Route path="/warehouses">
+          <Route index element={<WarehousesPage />} />
+          <Route path=":warehouseId" element={<WarehousesPage />} />
+        </Route>
+        <Route path="/inventory">
+          <Route index element={<InventoryPage />} />
+          <Route path="add" element={<InventoryForm />} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
       <Footer />
     </>
