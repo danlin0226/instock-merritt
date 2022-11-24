2f4b409876c7fab2847b24741dc71b7e1b31171d
wilsonn-g <wilson-gee@hotmail.com>
2022-11-24 08:16:00 -0800

    fix small styling

--- src/components/inventory-form/InventoryForm.jsx
+++ src/components/inventory-form/InventoryForm.jsx
@@ -1,16 +1,17 @@
+import "../../styles/partials/_typography.scss";
 import "./InventoryForm.scss";
 
 const InventoryForm = () => {
   return (
     <form className="inventory__form">
       <div className="item__details">
-        <h1 className="item__section-header">Item Details</h1>
+        <h2 className="subheader">Item Details</h2>
         <div className="item__container">
-          <label className="item__label">Item Name</label>
+          <label className="label-text">Item Name</label>
           <input className="item__form item__name" type="text" name="itemName" placeholder="Item Name" />
         </div>
         <div className="item__container">
-          <label className="item__label">Description</label>
+          <label className="label-text">Description</label>
           <textarea
             className="item__form item__description"
             type="text"
@@ -19,7 +20,7 @@ const InventoryForm = () => {
           />
         </div>
         <div className="item__container">
-          <label className="item__label">Category</label>
+          <label className="label-text">Category</label>
           <select className="item__form" defaultValue="">
             <option className="item__select" value="" disabled hidden>
               Please select
@@ -37,35 +38,30 @@ const InventoryForm = () => {
         </div>
       </div>
       <div className="item__availability">
-        <h1 className="item__section-header">Item Availability</h1>
+        <h2 className="subheader">Item Availability</h2>
         <div className="item__container">
-          <label className="item__label">Status</label>
+          <label className="label-text">Status</label>
           <div className="item__radio-container">
-            <input
-              className="item__label item__radio"
-              type="radio"
-              id="inStock"
-              name="itemStatus"
-              value="In stock"
-              checked
-            />
-              <label htmlFor="inStock">In stock</label>
-            <input
-              className="item__label item__radio"
-              type="radio"
-              id="outOfStock"
-              name="itemStatus"
-              value="Out of stock"
-            />
-              <label htmlFor="outOfStock">Out of stock</label>
+            <div className="item__radio">
+              <input type="radio" id="inStock" name="itemStatus" value="In stock" checked />
+              <label className="label-text item__radio-label" htmlFor="inStock">
+                In stock
+              </label>
+            </div>
+            <div className="item__radio">
+              <input type="radio" id="outOfStock" name="itemStatus" value="Out of stock" />
+              <label className="label-text item__radio-label" htmlFor="outOfStock">
+                Out of stock
+              </label>
+            </div>
           </div>
         </div>
         <div className="item__container">
-          <label className="item__label">Quantity</label>
+          <label className="label-text">Quantity</label>
           <input className="item__form item__quantity" type="text" name="itemQuantity" placeholder="0" />
         </div>
         <div className="item__container">
-          <label className="item__label">Warehouse</label>
+          <label className="label-text">Warehouse</label>
           <select className="item__form" defaultValue="">
             <option className="item__select" value="" disabled hidden>
               Please select
