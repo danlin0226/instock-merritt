c29d59684adaa3a615abc4145de4fcd0d4c3f06e
Allan Somera <nagal.somera@gmail.com>
2022-11-25 21:57:24 -0800

    restarted styling headers

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,19 +1,16 @@
 import React from "react";
 // import { Link } from "react-router-dom";
-import logo from "../../assets/Logo/InStock-Logo_1x.png";
 import "./Header.scss";
 
 export default function Header() {
   return (
     <div className="header">
-      <img className="header__logo" src={logo} alt="InStock Logo" />
-      <ul className="header__link-wrapper">
-        {/* <Link> */}
-        <li className="header__link">Warehouse</li>
-        {/* </Link>
-        <Link> */}
+      <div className="header__con1">
+        <div className="header__img-con"></div>
+      </div>
+      <ul className="header__con2">
+        <li className="header__link selected">Warehouse</li>
         <li className="header__link">Inventory</li>
-        {/* </Link> */}
       </ul>
     </div>
   );
