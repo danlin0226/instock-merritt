a6bb1a1bba9dd32ffae0e7d89fb346002e0ee65d
super_fox <super_fox@mail.ru>
2022-11-28 13:08:34 -0800

    font size/weight fix

--- src/components/buttonSet/ButtonSet.jsx
+++ src/components/buttonSet/ButtonSet.jsx
@@ -2,15 +2,12 @@ import "./ButtonSet.scss";
 import React from "react";
 import {useNavigate} from "react-router-dom";
 import Button from "../button/Button";
+import "../../styles/partials/_typography.scss";
 
 function ButtonSet(props) {
   const navigate = useNavigate();
   return (
     <section className="warehouse__buttons">
-      {/* <button className="warehouse__buttons__cancel">Cancel</button>
-      <button type="submit" className="warehouse__buttons__submit">
-        {props ? props.buttonName : `+ Add Warehouse`}
-      </button> */}
       <Button
         buttonText="Cancel"
         additionalClasses="warehouse__buttons__cancel"
