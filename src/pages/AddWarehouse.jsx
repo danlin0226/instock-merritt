2819eef7d1bd26755bf18175d3e019be0c616133
super_fox <super_fox@mail.ru>
2022-11-28 14:24:06 -0800

    footer fix + junk removed

--- src/pages/AddWarehouse.jsx
+++ src/pages/AddWarehouse.jsx
@@ -2,13 +2,12 @@ import React from "react";
 import ButtonSet from "../components/buttonSet/ButtonSet";
 import WarehouseForm from "../components/warehouseForm/WarehouseForm";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 const URL = "http://localhost:8080/warehouses";
 
 const AddWarehouse = ({ setWarehouses, warehouses }) => {
-  // const [warehouses, setWarehouses] = useState([]);
   const navigate = useNavigate();
 
   useEffect(() => {
